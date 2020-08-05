11909bf2a3ba498de891a9222a93b11cdef13d07
Tomas <tomasfreire@gmail.com>
2020-08-05 00:44:56 -0300

    fixed cors issue

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require("cors")
 
 
 mongoose
@@ -49,6 +50,10 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:8000']
+}))
 
 
 const index = require('./routes/index');
